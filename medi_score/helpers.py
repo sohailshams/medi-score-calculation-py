74d9cc47df61c7d3c496c98bbdd8e63d7a288899
sohailshams <sohailshamas@yahoo.com>
2023-10-06 23:08:13 +0100

    Added get_respiration_range_score function and get_respiration_range_score test suite.

--- medi_score/helpers.py
+++ medi_score/helpers.py
@@ -18,3 +18,21 @@ def get_consciousness_score(consciousness):
         case _:
             return False
         
+def get_respiration_range_score(respiration_range):
+    # Check if respiration is a number
+    if type(respiration_range) != int:
+        return False
+  
+    if respiration_range <= 8:
+        return 3
+    elif respiration_range >= 9 and respiration_range <= 11:
+        return 1
+    elif respiration_range >= 12 and respiration_range <= 20:
+        return 0
+    elif respiration_range >= 21 and respiration_range <= 24:
+        return 2
+    elif respiration_range >= 25:
+        return 3
+    else:
+        return False
+
--- tests/medi_score_tests.py
+++ tests/medi_score_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from medi_score.medi_score import medi_score_calculation
-from medi_score.helpers import get_air_or_oxygen_score, get_consciousness_score
+from medi_score.helpers import get_air_or_oxygen_score, get_consciousness_score, get_respiration_range_score
 from medi_score.Enums import AirOrOxygen, Consciousness
 
 #                                    -- medi_score_calculation tests --
@@ -54,6 +54,58 @@ class ConsciousnessTest(unittest.TestCase):
         self.assertEqual(alert_result, 0)
         cvpu_result = get_consciousness_score(Consciousness.CVPU.value)
         self.assertEqual(cvpu_result, 3)
+
+
+#                                    -- helper function - get_respiration_range_score tests --
+class RespirationRangeTest(unittest.TestCase):
+    """ Test module for get_respiration_range_score function """
+    
+    def test_get_respiration_range_score_returns_false_if_passed_string_value(self):
+        """ Confirm get_respiration_range_score returns False if a string is passed """
+        result = get_respiration_range_score("something")
+        self.assertFalse(result)
+    
+    def test_get_respiration_range_score_returns_correct_score_if_observation_range_less_equal_8(self):
+        """ Confirm get_respiration_range_score returns correct score observation range is <=8 """
+        result_less_8 = get_respiration_range_score(5)
+        result_equal_8 = get_respiration_range_score(8)
+        self.assertEqual(result_less_8, 3)
+        self.assertEqual(result_equal_8, 3)
+
+    def test_get_respiration_range_score_returns_correct_score_if_observation_range_9_11(self):
+        """ Confirm get_respiration_range_score returns correct score observation range is >=9 and <=11 """
+        result_equal_9 = get_respiration_range_score(9)
+        result_equal_10 = get_respiration_range_score(10)
+        result_equal_11 = get_respiration_range_score(11)
+        self.assertEqual(result_equal_9, 1)
+        self.assertEqual(result_equal_10, 1)
+        self.assertEqual(result_equal_11, 1)
+
+    def test_get_respiration_range_score_returns_correct_score_if_observation_range_12_20(self):
+        """ Confirm get_respiration_range_score returns correct score observation range is >=12 and <=20 """
+        result_equal_12 = get_respiration_range_score(12)
+        result_equal_15 = get_respiration_range_score(15)
+        result_equal_20 = get_respiration_range_score(20)
+        self.assertEqual(result_equal_12, 0)
+        self.assertEqual(result_equal_15, 0)
+        self.assertEqual(result_equal_20, 0)
+
+    def test_get_respiration_range_score_returns_correct_score_if_observation_range_21_24(self):
+        """ Confirm get_respiration_range_score returns correct score observation range is >=21 and <=24 """
+        result_equal_21 = get_respiration_range_score(21)
+        result_equal_22 = get_respiration_range_score(22)
+        result_equal_24 = get_respiration_range_score(24)
+        self.assertEqual(result_equal_21, 2)
+        self.assertEqual(result_equal_22, 2)
+        self.assertEqual(result_equal_24, 2)
+
+    def test_get_respiration_range_score_returns_correct_score_if_observation_range_greater_equal_25(self):
+        """ Confirm get_respiration_range_score returns correct score observation range is >=25 """
+        result_equal_25 = get_respiration_range_score(25)
+        result_greater_25 = get_respiration_range_score(30)
+        self.assertEqual(result_equal_25, 3)
+        self.assertEqual(result_greater_25, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
